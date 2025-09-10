1d12e07e04f217265c96b78df1dba379f303771b
Jdutra <dutrajully@icloud.com>
2025-09-10 19:52:55 -0400

    update formatPrice

--- infrastructure/frontend/src/utils/formatPrice.jsx
+++ infrastructure/frontend/src/utils/formatPrice.jsx
@@ -4,9 +4,12 @@
  * @returns {string} - Valor formatado como R$
  */
 export const formatPrice = (value) => {
-  const roundedValue = Number(value.toFixed(2));
+  const roundedValue = Number.parseFloat(value); 
+
   return new Intl.NumberFormat('pt-BR', {
     style: 'currency',
-    currency: 'BRL'
+    currency: 'BRL',
+    minimumFractionDigits: 2, 
+    maximumFractionDigits: 2
   }).format(roundedValue);
 };
